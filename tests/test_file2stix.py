2c8080e78d402a65b0145bbd1d3f06ec59e9d286
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2022-09-03 10:40:44 +0530

    Update unit tests
    Resolves  #34
    
    Signed-off-by: Shanthanu S Rai <shanthanu.s.rai9@gmail.com>

--- tests/test_file2stix.py
+++ tests/test_file2stix.py
@@ -1,67 +1,117 @@
+"""
+Run below command to run all tests (in repo root)
+```
+pytest -rP -vv
+```
+
+Run below command to update test reports
+```
+pytest -rP -vv --update-expected-reports
+```
+"""
+
 import json
 import pytest
+import os
 import file2stix.main
 from file2stix.config import Config
+from file2stix.observables import (
+    MITREEnterpriseAttackObservable, 
+    MITRECapecObservable, 
+    MITREICSAttackObservable, 
+    MITREMobileAttackObservable
+)
 
-# It's a tuple containing example program path and the corresponding expected reports
-testdata = [
-    (
-        "examples/FireEyeAPT39.txt",
-        "tests/expected_reports/bundle--8648ec6a-a38e-49cc-ad7e-b4978d591bdb.json",
-    ),
-    (
-        "examples/input2.txt",
-        "tests/expected_reports/bundle--57eee019-c92e-428d-a7a5-4142828f7892.json",
-    ),
-    (
-        "examples/input.txt",
-        "tests/expected_reports/bundle--b938e95a-f3d7-4ca0-bb17-fc5ed9a6c817.json",
-    ),
-    # Ignoring madiant-api.txt for now since it takes a lot of time
-    # (
-    #     "examples/mandiant-apt1.txt",
-    #     "tests/expected_reports/bundle--8a6989a9-d1a1-474b-b792-1c82f4cdb931.json",
-    # ),
+slow_tests = [
+    "country_adjective.txt", "country_code.txt", "domain.txt"
 ]
 
+# It's a tuple containing test program path and the corresponding expected reports
+testdata = []
 
-@pytest.mark.parametrize(
-    "example_path,expected_report_path", testdata, ids=[t[0] for t in testdata]
-)
-def test_file2stix_cli(example_path, expected_report_path):
+OBSERVABLE_TEST_FOLDER = "tests/observable_tests/"
+for (_, _, filenames) in os.walk(OBSERVABLE_TEST_FOLDER):
+    for filename in filenames:
+        testdata.append(OBSERVABLE_TEST_FOLDER + filename)
+
+
+for test in testdata[:]:
+    for slow_test in slow_tests:
+        if slow_test in test:
+            testdata.remove(test)
+
+@pytest.mark.parametrize("test_file_path", testdata, ids=testdata)
+def test_file2stix_cli(test_file_path, update_expected_reports):
     """
-    Run file2stix-cli tool for example program and compare the 
+    Run file2stix-cli tool for example program and compare the
     generated report with the expected report.
     """
-    config = Config(example_path, update_mitre_cti_database=True)
+    print(update_expected_reports)
+
+    test_file_name = os.path.basename(test_file_path)
+    expected_report_path = "tests/expected_reports/" + test_file_name + ".json"
+    output_json_file_path = None
+    update_mitre_cti_database = False
+    ignore_observables_list = [
+        MITREEnterpriseAttackObservable, 
+        MITRECapecObservable, 
+        MITREICSAttackObservable, 
+        MITREMobileAttackObservable,
+    ]
+
+    config = None
+    if update_expected_reports:
+        output_json_file_path = expected_report_path
+
+    if "mitre" in test_file_path:
+        update_mitre_cti_database = True
+        ignore_observables_list = None
+
+
+    config = Config(
+        test_file_path,
+        update_mitre_cti_database=update_mitre_cti_database,
+        output_json_file_path=output_json_file_path,
+        ignore_observables_list=ignore_observables_list
+    )
+
     report_path = file2stix.main.main(config)
-    
-    with open(report_path) as f:
-        report = json.load(f)
 
-    with open(expected_report_path) as f:
-        expected_report = json.load(f)
+    if not update_expected_reports:
+        with open(report_path) as f:
+            report = json.load(f)
 
-    for sdo_object, expected_sdo_object in zip(
-        report["objects"], expected_report["objects"]
-    ):
-        assert sdo_object["type"] == expected_sdo_object["type"]
-        assert sdo_object["spec_version"] == expected_sdo_object["spec_version"]
-        assert sdo_object["name"] == expected_sdo_object["name"]
+        with open(expected_report_path) as f:
+            expected_report = json.load(f)
 
-        if "indicator_types" in expected_sdo_object:
-            assert (
-                len(sdo_object["indicator_types"])
-                == len(expected_sdo_object["indicator_types"])
-                == 1
-            )
-            assert sdo_object["indicator_types"][0] == expected_sdo_object["indicator_types"][0]
-        
-        if "pattern" in expected_sdo_object:
-            assert sdo_object["pattern"] == expected_sdo_object["pattern"]
+        for sdo_object, expected_sdo_object in zip(
+            report["objects"], expected_report["objects"]
+        ):
+            assert sdo_object["type"] == expected_sdo_object["type"]
+            assert sdo_object["spec_version"] == expected_sdo_object["spec_version"]
 
-        if "pattern_type" in expected_sdo_object:
-            assert sdo_object["pattern_type"] == expected_sdo_object["pattern_type"]
+            if "name" in expected_sdo_object:
+                assert sdo_object["name"] == expected_sdo_object["name"]
 
-        if "pattern_version" in expected_sdo_object:
-            assert sdo_object["pattern_version"] == expected_sdo_object["pattern_version"]
+            if "indicator_types" in expected_sdo_object:
+                assert (
+                    len(sdo_object["indicator_types"])
+                    == len(expected_sdo_object["indicator_types"])
+                    == 1
+                )
+                assert (
+                    sdo_object["indicator_types"][0]
+                    == expected_sdo_object["indicator_types"][0]
+                )
+
+            if "pattern" in expected_sdo_object:
+                assert sdo_object["pattern"] == expected_sdo_object["pattern"]
+
+            if "pattern_type" in expected_sdo_object:
+                assert sdo_object["pattern_type"] == expected_sdo_object["pattern_type"]
+
+            if "pattern_version" in expected_sdo_object:
+                assert (
+                    sdo_object["pattern_version"]
+                    == expected_sdo_object["pattern_version"]
+                )
